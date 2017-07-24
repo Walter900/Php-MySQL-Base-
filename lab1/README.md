94cff44291a9b5ecb437411cc733012b64596c13
Walter900 <szjmfeng@gmail.com>
2017-07-24 14:01:09 -0700

    lab1

---
+++ lab1/README.md
@@ -0,0 +1,3 @@
+                    lab1
+   There are four file dataBase.php, index.php, mysql.txt, 
+and signup.php. 
---
+++ lab1/index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+
+<h1>First Lab</h1>
+
+<form action = "signup.php" method = "POST">
+     <input type = "text" name= "first" placeholder = "Firstname">
+     <br>
+     <input type = "text" name= "last" placeholder = "Lastname">
+     <br>
+     <input type = "text" name= "email" placeholder = "E-mail">
+     <br>
+     <input type = "text" name= "uid" placeholder = "Username">
+     <br>
+     <input type = "password" name= "pwd" placeholder = "Password">
+     <br>
+     <button type = "submit" name = "submit">Sign up</button>
+</form>	
+
+
+</body>
+</html>
