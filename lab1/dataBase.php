a435588a1d1fe243caa79c0b10841a5d843115d9
Walter900 <szjmfeng@gmail.com>
2017-07-26 16:18:13 -0700

    add some commits

--- lab1/dataBase.php
+++ lab1/dataBase.php
@@ -5,6 +5,7 @@ $dbUsername = "root";
 $dbPassword = "";
 $dbName = "loginsystem";
 
+//connect to the database
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 ?>
--- lab1/signup.php
+++ lab1/signup.php
@@ -8,9 +8,15 @@
  	$uid = mysqli_real_escape_string($conn,$_POST['uid']);
  	$pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
 
+    //the command to the database,
+    //the behavior to the database
 	$sql = "INSERT INTO users(user_first, user_last, user_email, user_uid, user_pwd)
    		 VALUES('$first', '$last', '$email', '$uid', '$pwd');";
     
+    //mysqli_query($a, $b) : Issue a query on a MySQL database,
+    //which often involves storing or retrieving data from a table.		 
+    //$a : the name or the information of the database
+    //$b : the way to manipulate the database 
     mysqli_query($conn, $sql);
 
     header("Location: index.php?signup=success");
